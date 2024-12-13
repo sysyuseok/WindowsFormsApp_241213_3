3393b3584eb9ce7390b0cc4b5a264151ed8bda8e
johnAtSpreatics <john@spreatics.com>
2024-12-13 12:33:33 +0900

    Add project files.

---
+++ WindowsFormsApp_241213/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_241213
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MessageBox.Show("하하하하하");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
