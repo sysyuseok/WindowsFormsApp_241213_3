85f2da16098cd066441105eac4dc5dba1696b6ab
sysyuseok <sonyous0513@naver.com>
2024-12-16 10:56:26 +0900

    add a line

--- WindowsFormsApp_241213/yuseok.cs
+++ WindowsFormsApp_241213/yuseok.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp_241213
 {
     internal class yuseok
     {
         //yuseok
+        string name = "yuseok";
+        
     }
+
 }
